c34f00ac9bce4c517cdaa479b69aa43799ba9ded
Sukiie <90098528+Sukiie@users.noreply.github.com>
2021-09-14 23:15:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,19 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+client.on('messageReactionAdd', (react, user) => {
+  let guildID = "885924068287741963";
+  let msgID = "887441843531419649";
+  let idReact = "887446166424399912";
+
+  if (react.emoji.id == idReact && react.message.guild.id == guildID && react.message.id == msgID) {
+    const guild = client.guilds.cache.get(react.message.guild.id);
+    const member = guild.members.cache.get(user.id);
+    const role = guild.roles.cache.get("885924068287741966");
+    member.roles.add(role)
+    member.user.send("Votre rôle vous a bien été attribuez !");
+  }
+
+})
+
 client.login(process.env.TOKEN);
